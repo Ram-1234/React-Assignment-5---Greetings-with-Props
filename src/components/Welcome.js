624d5dc7d56afd25a92851ea510aaac8d2c764dc
Ram-1234 <ramnayan699@gmail.com>
2021-03-31 18:23:28 +0530

    modified

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,10 +4,10 @@ import React from 'react';
 function Welcome(props){
     return (
         <>
-        <h1>Hey {props.name} !</h1>
-        <h2>Welcome to Newton School.</h2>
+        <h1>Hey {props.name}!</h1>
+        <h2> Welcome to Newton School. </h2>
         </>
-    )
+    );
 }
 
 export default Welcome;
